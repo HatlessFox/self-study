fb666cac9d11a41deb6018dafa38b391b02a422c
HatlessFox <hatless.fox@gmail.com>
2015-10-30 15:34:03 +0300

    [AnsiCL] Fix ch.5-9 task

--- lisp/ansi_common_lisp-graham/ch_5/task_9.lisp
+++ lisp/ansi_common_lisp-graham/ch_5/task_9.lisp
@@ -1,8 +1,5 @@
 (defconstant +net+ '((a b c) (b c) (c d)))
 
-(defun new-paths (path node net)
-  (mapcar #'(lambda (n) (cons n path)) (cdr (assoc node net))))
-
 ;;; catch-throw
 
 (defun shortest-path-ct (s e net)
@@ -13,12 +10,27 @@
   (if (null q) nil
     (let* ((path (car q))
 	   (node (car path)))
-      (if (eql node e) (throw 'result (reverse path))
-	(bfs-ct e (append (cdr q) (new-paths path node net)) net)))))
+      (if (eql node e) (reverse path)
+	(bfs-ct e (append (cdr q) (new-paths-ct path node net e)) net)))))
+
+
+(defun new-paths-ct (path node net e)
+  (mapcar #'(lambda (n)
+	      (if (eq n e)
+		  (throw 'result (reverse (cons n path)))
+		(cons n path))) (cdr (assoc node net))))
+
 
 ;;(shortest-path-ct 'a 'd +net+)
 
 ;; w/o catch-throw
+
+(defun new-paths (path node net e)
+  (values nil (mapcar
+	       #'(lambda (n)
+		   (if (eq n e)
+		       (return-from new-paths (values t (reverse (cons n path))))
+		     (cons n path))) (cdr (assoc node net)))))
 
 (defun shortest-path (s e net) (bfs e (list (list s)) net))
 
@@ -26,5 +38,9 @@
   (if (null q) nil
     (let* ((path (car q))
 	   (node (car path)))
-      (if (eql node e) (return-from bfs (reverse path))
-	(bfs e (append (cdr q) (new-paths path node net)) net)))))
+      (if (eql node e) (reverse path)
+	(let* ((new-paths (multiple-value-list (new-paths path node net e)))
+	       (found (car new-paths))
+	       (paths (cadr new-paths)))
+	  (if found paths
+	    (bfs e (append (cdr q) paths) net)))))))
