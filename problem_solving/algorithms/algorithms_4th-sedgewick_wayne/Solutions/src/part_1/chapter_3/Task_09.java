8b511cc2795700d28799b7dce7563d4734ad8f63
HatlessFox <hatless.fox@gmail.com>
2016-02-15 14:35:48 +0300

    [SedjAlg] Add 8-10 tasks from section 1.3 and Tester utility

---
+++ problem_solving/algorithms/algorithms_4th-sedgewick_wayne/Solutions/src/part_1/chapter_3/Task_09.java
@@ -0,0 +1,79 @@
+package part_1.chapter_3;
+
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
+/*
+ * Task 1.3.9
+ * 
+ * Write a program that takes from standard input an expression without left parentheses and prints 
+ * the equivalent infix expression with the parentheses inserted. For example, given the input:
+ *   1 + 2 ) * 3 - 4 ) * 5 - 6 ) ) ) ==> ( ( 1 + 2 ) * ( ( 3 - 4 ) * ( 5 - 6 ) ) )
+ *   
+ * NB: According to p. 128 expression is defined as: number OR ( expression operator expression )
+ */
+public class Task_09 {
+    
+    /* Implementation */
+    
+    private static enum ElemType {
+        RPARAN, NUMBER, OP;
+        
+        public static ElemType fromString(String value) {
+            switch (value) {
+            case ")": return RPARAN;
+            case "+": case "*": case "/": case "-": case "%": return OP;
+            default: return NUMBER;
+            }
+        }
+    }
+    
+    private static int readExpression(String[] tokens, int i, Stack<String> dst) {
+        switch (ElemType.fromString(tokens[i])) {
+        case RPARAN:
+            dst.push(tokens[i]);
+            i = readExpression(tokens, i - 1, dst);
+            dst.push(tokens[i]); // operator
+            i = readExpression(tokens, i - 1, dst);
+            dst.push("("); // fix left parenthesis
+            return i;
+        case OP:
+            throw new RuntimeException("Op token is not expected");
+        case NUMBER:
+            dst.push(tokens[i]);
+            return i - 1;
+        }
+        throw new RuntimeException("Unreachable end of readExpression");
+    }
+    
+    private static String addLeftParentheses(String raw_data) {
+        Stack<String> holder = new Stack<>();
+        String[] tokens = raw_data.split("\\s");
+        readExpression(tokens, tokens.length - 1, holder);
+        
+        StringBuilder sb = new StringBuilder();
+        holder.forEach(s -> { sb.append(s); sb.append(" "); });
+        return sb.toString().trim();
+    }
+    
+    /* Tests */
+    
+    private static void runTest(int test_id, String input, String expected_output) {
+        String output = addLeftParentheses(input);
+        if (expected_output.equals(output)) {
+            StdOut.printf("%d. Valid \n", test_id);
+        } else {
+            StdOut.printf("%d. Invalid. Output: %s\n",
+                    test_id, output.isEmpty() ? "<empty>" : output);
+        }
+    }
+    
+    private static void __runTests() {
+        runTest(0, "1 + 2 ) * 3 - 4 ) * 5 - 6 ) ) )",
+                   "( ( 1 + 2 ) * ( ( 3 - 4 ) * ( 5 - 6 ) ) )");
+    }
+    
+    public static void main(String[] args) {
+        __runTests();
+    }
+}
---
+++ problem_solving/algorithms/algorithms_4th-sedgewick_wayne/Solutions/src/utils/Tester.java
@@ -0,0 +1,25 @@
+package utils;
+
+import java.util.function.Function;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Tester<T, R> {
+
+    private int test_id;
+    private Function<T, R> testee;
+    
+    public Tester(Function<T, R> testee) { this.testee = testee; }
+    
+    public void runTest(T input, R expected_output) {
+        StdOut.println(runTest(test_id++, testee, input, expected_output));
+    }
+    
+    public static <T, R> String runTest(int test_id, Function<T, R> testee,
+                                        T input, R expected_output) {
+        R output = testee.apply(input);
+        String test_resut = output.equals(expected_output) ? "OK" :
+            String.format("FAILED.\n %s vs %s", output, expected_output);
+        return String.format("%d. %s", test_id, test_resut);
+    }
+}
