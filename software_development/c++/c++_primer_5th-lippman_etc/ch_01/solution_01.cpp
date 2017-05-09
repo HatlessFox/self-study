1867431be34f06ee9946fcb1f882ad42ff1b8398
HatlessFox <hatless.fox@gmail.com>
2017-05-09 23:18:58 +0300

    [C++][Primer5] 1.1, 1.2 solns

--- software_development/c++/c++_primer_5th-lippman_etc/ch_01/solution_01.cpp
+++ software_development/c++/c++_primer_5th-lippman_etc/ch_01/solution_01.cpp
@@ -1,8 +1,9 @@
 /*
-  Excercise 1.1, page 5.
- 
-  Review the documentation for your compiler and determine what file naming convention it uses. Compile and run the main program from page 2.
-*/
+ *  Excercise 1.1, page 5.
+ *
+ * Review the documentation for your compiler and determine what file naming
+ * convention it uses. Compile and run the main program from page 2.
+ */
 
 int main(int, char**) {
   return 0;
